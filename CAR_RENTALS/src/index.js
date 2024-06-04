58c2394e1407610a187685621bff49daf74df5a3
Keith <keithmwanandimai@gmail.com>
2024-06-04 10:54:02 +0100

    DONE

--- CAR_RENTALS/src/index.js
+++ CAR_RENTALS/src/index.js
@@ -1,6 +1,10 @@
-const express = require("express")
-const app = express()
-const path = requre("path")
+const express=require("express")
+const app=express()
+const path=requre("path")
+
+app.get("/",(req,res)=>{
+    res.render("login")
+} )
 
 app.listen(3000,()=>{
     console.log("port connected");
